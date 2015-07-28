a3867b8f27399d891d0a0974a83da2ffd35b5c94
Cillian Myles <mylsey4thewin@gmail.com>
2015-07-28 11:59:05 +0100

    Editing logs for Driver class.

--- src/main/java/com/onepagecrm/Driver.java
+++ src/main/java/com/onepagecrm/Driver.java
@@ -4,12 +4,15 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
+import java.util.logging.Logger;
 
 import com.onepagecrm.models.User;
 import com.onepagecrm.net.ApiClient;
 
 public class Driver {
 
+	private static final Logger LOG = Logger.getLogger(Driver.class.getName());
+	
 	public static void main(String[] args) {
 		
 		Properties prop = new Properties();
@@ -21,14 +24,16 @@ public class Driver {
 			// Load the properties file
 			prop.load(input);
 	 
-		} catch (IOException ex) {
-			ex.printStackTrace();
+		} catch (IOException e) {
+			LOG.severe("Error loading the config.properties file");
+			LOG.severe(e.toString());
 		} finally {
 			if (input != null) {
 				try {
 					input.close();
 				} catch (IOException e) {
-					e.printStackTrace();
+					LOG.severe("Error closing the config.properties file");
+					LOG.severe(e.toString());
 				}
 			}
 		}
