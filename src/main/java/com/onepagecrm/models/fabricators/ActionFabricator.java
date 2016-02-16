9c11c9de30fe5b6f507d8ca4c852c0192d2f8af3
Cillian <mylsey4thewin@gmail.com>
2016-02-16 12:07:24 +0000

    Adding Action fabricator for mobile app database testing.

---
+++ src/main/java/com/onepagecrm/models/fabricators/ActionFabricator.java
@@ -0,0 +1,44 @@
+package com.onepagecrm.models.fabricators;
+
+import com.onepagecrm.models.Action;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Cillian Myles <cillian@onepagecrm.com> on 16/02/2016.
+ */
+public class ActionFabricator extends BaseFabricator {
+
+    public static Action single() {
+        return new Action()
+                .setId("5694e1999007ba687b0000ca")
+                .setContactId("5694e1999007ba687b0000c9")
+                .setText("Call Java")
+                .setAssigneeId("559cd1866f6e656707000001")
+                .setStatus("asap");
+    }
+
+    public static List<Action> list() {
+        List<Action> actions = new ArrayList<>();
+        actions.add(new Action()
+                .setId("5694e1999007ba687b0000ca")
+                .setContactId("5694e1999007ba687b0000c9")
+                .setText("Call Java")
+                .setAssigneeId("559cd1866f6e656707000001")
+                .setStatus("asap"));
+        actions.add(new Action()
+                .setId("5694e1959007ba687b000005")
+                .setContactId("5694e1959007ba687b000004")
+                .setText("Call Pawel")
+                .setAssigneeId("559cd1866f6e656707000001")
+                .setStatus("asap"));
+        actions.add(new Action()
+                .setId("5694e1999007ba687b0000c1")
+                .setContactId("5694e1999007ba687b0000c0")
+                .setText("Call to follow up with Tiger")
+                .setAssigneeId("559cd1866f6e656707000001")
+                .setStatus("asap"));
+        return actions;
+    }
+}
--- src/main/java/com/onepagecrm/models/fabricators/ContactFabricator.java
+++ src/main/java/com/onepagecrm/models/fabricators/ContactFabricator.java
@@ -41,6 +41,9 @@ public class ContactFabricator extends BaseFabricator {
         // Phone, Email, Web.
         addContactables(contact);
 
+        // Next Action.
+        contact.setNextAction(ActionFabricator.single());
+
         return contact;
     }
 
