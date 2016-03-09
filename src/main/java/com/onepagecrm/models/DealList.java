fd8d75e274996bb828e3748183d43d316e5b5856
Cillian Myles <mylsey4thewin@gmail.com>
2016-03-09 11:26:59 +0000

    Implementing DealList.

--- src/main/java/com/onepagecrm/models/DealList.java
+++ src/main/java/com/onepagecrm/models/DealList.java
@@ -1,74 +1,38 @@
 package com.onepagecrm.models;
 
+import com.onepagecrm.exceptions.OnePageException;
 import com.onepagecrm.models.serializers.DealSerializer;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by Cillian Myles (cillian@onepagecrm.com) on 11/24/15.
  */
-public class DealList extends ArrayList<Deal> implements Serializable {
+public class DealList extends ResourceList<Deal> implements Serializable {
 
-    private List<Deal> deals;
+    @Override
+    public DealList nextPage() throws OnePageException {
+        this.paginator.getNextPageNo();
+        return Account.loggedInUser.pipeline(paginator);
+    }
 
-    public DealList(List<Deal> deals) {
-        this.deals = new ArrayList<>();
-        if (deals != null && !deals.isEmpty()) {
-            for (int i = 0; i < deals.size(); i++) {
-                this.deals.add(deals.get(i));
-            }
-        }
+    @Override
+    public DealList refresh() throws OnePageException {
+        DealList list = Account.loggedInUser.pipeline();
+        this.setList(list);
+        return this;
     }
 
     public DealList() {
-        this.deals = new ArrayList<>();
+        super();
+    }
+
+    public DealList(List<Deal> deals) {
+        super(deals);
     }
 
     public String toString() {
         return DealSerializer.toJsonArray(this);
-    }
-
-    public List<Deal> getDeals() {
-        return deals;
-    }
-
-    public void setDeals(List<Deal> deals) {
-        this.deals = new ArrayList<>();
-        if (deals != null && !deals.isEmpty()) {
-            for (int i = 0; i < deals.size(); i++) {
-                this.deals.add(deals.get(i));
-            }
-        }
-    }
-
-    public boolean isEmpty() {
-        return deals.isEmpty();
-    }
-
-    public int size() {
-        return deals.size();
-    }
-
-    public void add(int index, Deal deal) {
-        deals.add(index, deal);
-    }
-
-    public boolean add(Deal deal) {
-        return deals.add(deal);
-    }
-
-    public Deal get(int index) {
-        return deals.get(index);
-    }
-
-    public Deal set(int index, Deal deal) {
-        deals.set(index, deal);
-        return deal;
-    }
-
-    public int indexOf(Deal deal) {
-        return deals.indexOf(deal);
     }
 }
