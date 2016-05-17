7f3bd78d015298c07e8196c45130e5cd48909226
Cillian Myles <mylsey4thewin@gmail.com>
2016-05-17 15:54:06 +0100

    Updating DealStageSerializer.

--- src/main/java/com/onepagecrm/models/serializers/DealStageSerializer.java
+++ src/main/java/com/onepagecrm/models/serializers/DealStageSerializer.java
@@ -10,14 +10,13 @@ import java.util.List;
 /**
  * Created by alex on 4/23/16.
  */
-public class DealStageSerializer extends BaseSerializer{
+public class DealStageSerializer extends BaseSerializer {
 
-
-    public static List<DealStage> fromJsonArray(JSONArray pDealStagesArray) {
+    public static List<DealStage> fromJsonArray(JSONArray dealStagesArray) {
         List<DealStage> lDealStages = new LinkedList<>();
-        if (pDealStagesArray!=null){
-            for (int i=0;i<pDealStagesArray.length();++i){
-                JSONObject lObject = pDealStagesArray.optJSONObject(i);
+        if (dealStagesArray != null) {
+            for (int i = 0; i < dealStagesArray.length(); ++i) {
+                JSONObject lObject = dealStagesArray.optJSONObject(i);
                 DealStage lDealStage = new DealStage();
                 lDealStage.setLabel(lObject.optString(DEAL_STAGE_LABEL_TAG));
                 lDealStage.setPercentage(lObject.optInt(DEAL_STAGE_STAGE_TAG));
