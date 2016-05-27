510ad0ddbd08efc069e3b9f4b2df773f8b4c6680
Daniilys <daniil.pozhidaev@gmail.com>
2016-05-27 18:40:36 +0300

    Fix close call phone serialization

--- src/main/java/com/onepagecrm/models/serializers/CallSerializer.java
+++ src/main/java/com/onepagecrm/models/serializers/CallSerializer.java
@@ -15,98 +15,99 @@ import java.util.logging.Logger;
 
 public class CallSerializer extends BaseSerializer {
 
-    private static final Logger LOG = Logger.getLogger(CallSerializer.class.getName());
-    private static final java.lang.String PHONE_NUMBER_TAG = "phone_number";
-    private static final java.lang.String RECORDING_LINK_TAG = "recording_link";
+	private static final Logger LOG = Logger.getLogger(CallSerializer.class.getName());
+	private static final java.lang.String PHONE_NUMBER_TAG = "phone_number";
+	private static final java.lang.String RECORDING_LINK_TAG = "recording_link";
 
-    public static Call fromString(String responseBody) throws OnePageException {
-        OnePageException exception;
+	public static Call fromString(String responseBody) throws OnePageException {
+		OnePageException exception;
 
-        try {
-            JSONObject responseObject = new JSONObject(responseBody);
-            JSONObject data = responseObject.optJSONObject(DATA_TAG);
-            return CallSerializer.objFromJson(data);
+		try {
+			JSONObject responseObject = new JSONObject(responseBody);
+			JSONObject data = responseObject.optJSONObject(DATA_TAG);
+			return CallSerializer.objFromJson(data);
 
-        } catch (ClassCastException e) {
-            exception = (OnePageException) BaseSerializer.fromString(responseBody);
-            throw exception;
+		} catch (ClassCastException e) {
+			exception = (OnePageException) BaseSerializer.fromString(responseBody);
+			throw exception;
 
-        } catch (JSONException e) {
-            LOG.severe("Could not find call tag");
-            LOG.severe(e.toString());
-        }
-        return new Call();
-    }
+		} catch (JSONException e) {
+			LOG.severe("Could not find call tag");
+			LOG.severe(e.toString());
+		}
+		return new Call();
+	}
 
-     public static String toJsonObject(Call call) {
-        JSONObject callObject = new JSONObject();
-        addJsonStringValue(call.getId(), callObject, ID_TAG);
-        addJsonStringValue(call.getText(), callObject, TEXT_TAG);
-        addJsonStringValue(call.getCallResult().getId(), callObject, CALL_RESULT_TAG);
-        addJsonLongValue(DateSerializer.dateInMillis(call.getTime()), callObject, CALL_TIME_INT_TAG);
-        addJsonStringValue(call.getContactId(), callObject, CONTACT_ID_TAG);
-        addJsonStringValue(DateSerializer.toFormattedDateTimeString(call.getCreatedAt()), callObject, CREATED_AT_TAG);
-        addJsonStringValue(DateSerializer.toFormattedDateTimeString(call.getModifiedAt()), callObject, MODIFIED_AT_TAG);
-        addJsonStringValue(call.getVia(), callObject, VIA_TAG);
-        addJsonStringValue(call.getAuthor(), callObject, AUTHOR_TAG);
+	public static String toJsonObject(Call call) {
+		JSONObject callObject = new JSONObject();
+		addJsonStringValue(call.getId(), callObject, ID_TAG);
+		addJsonStringValue(call.getText(), callObject, TEXT_TAG);
+		addJsonStringValue(call.getCallResult().getId(), callObject, CALL_RESULT_TAG);
+		addJsonLongValue(DateSerializer.dateInMillis(call.getTime()), callObject, CALL_TIME_INT_TAG);
+		addJsonStringValue(call.getContactId(), callObject, CONTACT_ID_TAG);
+		addJsonStringValue(DateSerializer.toFormattedDateTimeString(call.getCreatedAt()), callObject, CREATED_AT_TAG);
+		addJsonStringValue(DateSerializer.toFormattedDateTimeString(call.getModifiedAt()), callObject, MODIFIED_AT_TAG);
+		addJsonStringValue(call.getVia(), callObject, VIA_TAG);
+		addJsonStringValue(call.getAuthor(), callObject, AUTHOR_TAG);
+		addJsonStringValue(call.getPhoneNumber(), callObject, PHONE_NUMBER_TAG);
 //        addJsonStringValue(call.getAttachments(), callObject, ATTACHMENTS_TAG);
-        return callObject.toString();
-    }
+		return callObject.toString();
+	}
 
-    public static String toJsonArray(List<Call> calls) {
-        JSONArray callsArray = new JSONArray();
-        for (int i = 0; i < calls.size(); i++) {
-            try {
-                callsArray.put(new JSONObject(toJsonObject(calls.get(i))));
-            } catch (JSONException e) {
-                LOG.severe("Error creating JSONObject out of Call");
-                LOG.severe(e.toString());
-            }
-        }
-        return callsArray.toString();
-    }
+	public static String toJsonArray(List<Call> calls) {
+		JSONArray callsArray = new JSONArray();
+		for (int i = 0; i < calls.size(); i++) {
+			try {
+				callsArray.put(new JSONObject(toJsonObject(calls.get(i))));
+			} catch (JSONException e) {
+				LOG.severe("Error creating JSONObject out of Call");
+				LOG.severe(e.toString());
+			}
+		}
+		return callsArray.toString();
+	}
 
-    public static List<Call> listFromString(String pResponseBody) {
-        List<Call> lCalls = new LinkedList<>();
-        try{
-            JSONObject root = new JSONObject(pResponseBody);
-            JSONObject data = root.optJSONObject(DATA_TAG);
-            JSONArray calls = data.optJSONArray(CALLS_TAG);
-            for (int i=0;i<calls.length();++i){
-                JSONObject obj = calls.optJSONObject(i);
-                Call lCall = objFromJson(obj);
-                if (lCall!=null) {
-                    lCalls.add(lCall);
-                }
-            }
-        }catch (Exception e){
-            LOG.severe(e.toString());
-        }
-        return lCalls;
-    }
+	public static List<Call> listFromString(String pResponseBody) {
+		List<Call> lCalls = new LinkedList<>();
+		try {
+			JSONObject root = new JSONObject(pResponseBody);
+			JSONObject data = root.optJSONObject(DATA_TAG);
+			JSONArray calls = data.optJSONArray(CALLS_TAG);
+			for (int i = 0; i < calls.length(); ++i) {
+				JSONObject obj = calls.optJSONObject(i);
+				Call lCall = objFromJson(obj);
+				if (lCall != null) {
+					lCalls.add(lCall);
+				}
+			}
+		} catch (Exception e) {
+			LOG.severe(e.toString());
+		}
+		return lCalls;
+	}
 
-    private static Call objFromJson(JSONObject pObj) {
-        JSONObject lObject = pObj.optJSONObject("call");
-        if (lObject!=null){
-            Call lCall = new Call();
-            lCall.setId(lObject.optString(ID_TAG));
-            lCall.setVia(lObject.optString(VIA_TAG));
-            lCall.setAuthor(lObject.optString(AUTHOR_TAG));
-            lCall.setPhoneNumber(lObject.optString(PHONE_NUMBER_TAG));
-            lCall.setText(lObject.optString(TEXT_TAG));
-            String callResultId = lObject.optString(CALL_RESULT_TAG);
-            for (CallResult lCallResult:Account.loggedInUser.getAccount().getCallResults()){
-                if (lCallResult.getId().equals(callResultId)){
-                    lCall.setCallResult(lCallResult);
-                }
-            }
-            lCall.setTime(DateSerializer.fromNumString(lObject.optString(CALL_TIME_INT_TAG)));
-            lCall.setContactId(lObject.optString(CONTACT_ID_TAG));
-            lCall.setRecordingLink(lObject.optString(RECORDING_LINK_TAG));
-            lCall.setCreatedAt(DateSerializer.fromCallFormattedString(lObject.optString(CREATED_AT_TAG)));
-            lCall.setModifiedAt(DateSerializer.fromFormattedString(lObject.optString(MODIFIED_AT_TAG)));
-            return lCall;
-        }
-        return null;
-    }
+	private static Call objFromJson(JSONObject pObj) {
+		JSONObject lObject = pObj.optJSONObject("call");
+		if (lObject != null) {
+			Call lCall = new Call();
+			lCall.setId(lObject.optString(ID_TAG));
+			lCall.setVia(lObject.optString(VIA_TAG));
+			lCall.setAuthor(lObject.optString(AUTHOR_TAG));
+			lCall.setPhoneNumber(lObject.optString(PHONE_NUMBER_TAG));
+			lCall.setText(lObject.optString(TEXT_TAG));
+			String callResultId = lObject.optString(CALL_RESULT_TAG);
+			for (CallResult lCallResult : Account.loggedInUser.getAccount().getCallResults()) {
+				if (lCallResult.getId().equals(callResultId)) {
+					lCall.setCallResult(lCallResult);
+				}
+			}
+			lCall.setTime(DateSerializer.fromNumString(lObject.optString(CALL_TIME_INT_TAG)));
+			lCall.setContactId(lObject.optString(CONTACT_ID_TAG));
+			lCall.setRecordingLink(lObject.optString(RECORDING_LINK_TAG));
+			lCall.setCreatedAt(DateSerializer.fromCallFormattedString(lObject.optString(CREATED_AT_TAG)));
+			lCall.setModifiedAt(DateSerializer.fromFormattedString(lObject.optString(MODIFIED_AT_TAG)));
+			return lCall;
+		}
+		return null;
+	}
 }
