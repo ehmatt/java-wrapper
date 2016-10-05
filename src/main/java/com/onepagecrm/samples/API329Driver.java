2a6bb748efb85b9b7b210f96e25d574bfe95d56e
Cillian Myles <mylsey4thewin@gmail.com>
2016-10-05 14:32:06 +0100

    Checking if Google Login works on API end.

---
+++ src/main/java/com/onepagecrm/samples/API329Driver.java
@@ -0,0 +1,50 @@
+package com.onepagecrm.samples;
+
+import com.onepagecrm.OnePageCRM;
+import com.onepagecrm.net.request.GoogleLoginRequest;
+import com.onepagecrm.net.request.Request;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import java.util.logging.Logger;
+
+/**
+ * Created by Cillian Myles <cillian@onepagecrm.com> on 05/10/2016.
+ */
+public class API329Driver {
+
+    private static final Logger LOG = Logger.getLogger(API329Driver.class.getName());
+
+    public static void main(String[] args) throws Exception {
+        Properties prop = new Properties();
+        InputStream input = null;
+
+        try {
+            input = new FileInputStream("config.properties");
+
+            // Load the properties file
+            prop.load(input);
+
+        } catch (IOException e) {
+            LOG.severe("Error loading the config.properties file");
+            LOG.severe(e.toString());
+        } finally {
+            if (input != null) {
+                try {
+                    input.close();
+                } catch (IOException e) {
+                    LOG.severe("Error closing the config.properties file");
+                    LOG.severe(e.toString());
+                }
+            }
+        }
+
+        OnePageCRM.setServer(Request.DEV_SERVER);
+
+        final String oauthCode = "4/BxhylUmoVASDT3B2VAMQcGbigyIN3v_x5xyxHpdokWc";
+        Request googleLogin = new GoogleLoginRequest(oauthCode);
+        googleLogin.send();
+    }
+}
