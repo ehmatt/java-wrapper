ff5801fc16145bd0fc12a4f9984327ffb1a70e8f
Cillian Myles <mylsey4thewin@gmail.com>
2017-08-18 17:18:59 +0100

    Extending 500 error functionality.

--- src/main/java/com/onepagecrm/exceptions/ServerErrorException.java
+++ src/main/java/com/onepagecrm/exceptions/ServerErrorException.java
@@ -2,4 +2,14 @@ package com.onepagecrm.exceptions;
 
 public class ServerErrorException extends APIException {
 
+    private String responseBody;
+
+    public String getResponseBody() {
+        return responseBody;
+    }
+
+    public ServerErrorException setResponseBody(String responseBody) {
+        this.responseBody = responseBody;
+        return this;
+    }
 }
