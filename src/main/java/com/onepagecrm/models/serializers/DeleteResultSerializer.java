083fdb6c47066f9e45b17875f0284dccbebda76d
Cillian Myles <mylsey4thewin@gmail.com>
2017-11-23 17:16:45 +0000

    AND-686 Tidy up;

--- src/main/java/com/onepagecrm/models/serializers/DeleteResultSerializer.java
+++ src/main/java/com/onepagecrm/models/serializers/DeleteResultSerializer.java
@@ -1,7 +1,6 @@
 package com.onepagecrm.models.serializers;
 
 import com.onepagecrm.exceptions.APIException;
-import com.onepagecrm.exceptions.OnePageException;
 import com.onepagecrm.models.internal.DeleteResult;
 import com.onepagecrm.net.Response;
 
@@ -13,17 +12,5 @@ public class DeleteResultSerializer extends BaseSerializer {
     public static DeleteResult fromResponse(String resourceId, Response response) throws APIException {
         BaseSerializer.fromResponse(response);
         return new DeleteResult(resourceId, true);
-    }
-
-    // TODO: throw APIException instead of OnePageException
-    // TODO: eventually delete
-    public static DeleteResult fromString(String resourceId, String responseBody) throws OnePageException {
-        try {
-            BaseSerializer.fromString(responseBody);
-            return new DeleteResult(resourceId, true);
-
-        } catch (ClassCastException e) {
-            throw (OnePageException) BaseSerializer.fromString(responseBody);
-        }
     }
 }
