d0b1da2c7cf6cbddd6f85afa11239124ecd07e4b
Cillian Myles <mylsey4thewin@gmail.com>
2017-12-20 13:45:38 +0000

    AND-686 Action as example;

--- src/main/java/com/onepagecrm/samples/Driver.java
+++ src/main/java/com/onepagecrm/samples/Driver.java
@@ -3,6 +3,7 @@ package com.onepagecrm.samples;
 import com.onepagecrm.OnePageCRM;
 import com.onepagecrm.exceptions.OnePageException;
 import com.onepagecrm.models.Account;
+import com.onepagecrm.models.ActionList;
 import com.onepagecrm.models.CompanyList;
 import com.onepagecrm.models.ContactList;
 import com.onepagecrm.models.Countries;
@@ -50,28 +51,31 @@ public class Driver {
                 prop.getProperty("username"),
                 prop.getProperty("password"));
 
-        LOG.info("Logged in User : " + loggedInUser);
+        LOG.info("Logged in User: " + loggedInUser);
 
-        LOG.info("Team : " + Account.team);
-        LOG.info("Settings : " + Account.settings);
-        LOG.info("Statuses : " + loggedInUser.getAccount().statuses);
-        LOG.info("Lead Sources : " + loggedInUser.getAccount().leadSources);
-        LOG.info("Deal Stages : " + Account.settings.getDealStages());
-        LOG.info("Custom Fields : " + loggedInUser.getAccount().customFields);
-        LOG.info("Company Fields : " + loggedInUser.getAccount().companyFields);
-        LOG.info("Deal Fields : " + loggedInUser.getAccount().dealFields);
-        LOG.info("Predefined Actions : " + loggedInUser.getAccount().predefinedActions);
-        LOG.info("Call Results : " + loggedInUser.getAccount().callResults);
-        LOG.info("Filters : " + loggedInUser.getAccount().filters);
-        LOG.info("Contacts counts : " + loggedInUser.getAccount().contactsCount);
-        LOG.info("Stream count : " + loggedInUser.getAccount().streamCount);
-        LOG.info("Contact Titles : " + loggedInUser.getAccount().contactTitles);
-        LOG.info("Account Rights : " + loggedInUser.getAccountRights());
+        LOG.info("Team: " + Account.team);
+        LOG.info("Settings: " + Account.settings);
+        LOG.info("Statuses: " + loggedInUser.getAccount().statuses);
+        LOG.info("Lead Sources: " + loggedInUser.getAccount().leadSources);
+        LOG.info("Deal Stages: " + Account.settings.getDealStages());
+        LOG.info("Custom Fields: " + loggedInUser.getAccount().customFields);
+        LOG.info("Company Fields: " + loggedInUser.getAccount().companyFields);
+        LOG.info("Deal Fields: " + loggedInUser.getAccount().dealFields);
+        LOG.info("Predefined Actions: " + loggedInUser.getAccount().predefinedActions);
+        LOG.info("Call Results: " + loggedInUser.getAccount().callResults);
+        LOG.info("Filters: " + loggedInUser.getAccount().filters);
+        LOG.info("Contacts counts: " + loggedInUser.getAccount().contactsCount);
+        LOG.info("Stream count: " + loggedInUser.getAccount().streamCount);
+        LOG.info("Contact Titles: " + loggedInUser.getAccount().contactTitles);
+        LOG.info("Account Rights: " + loggedInUser.getAccountRights());
 
         ContactList stream = loggedInUser.actionStream();
         ContactList contacts = loggedInUser.contacts();
         DealList pipeline = loggedInUser.pipeline();
         CompanyList companies = loggedInUser.companies();
         Countries countries = Countries.list();
+        ActionList actions = loggedInUser.actions();
+
+        LOG.info("Actions: " + actions);
     }
 }
